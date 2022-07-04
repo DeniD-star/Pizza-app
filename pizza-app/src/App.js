ee1df7bf00c83b4391bf76c8d0cd4c491860c2a7
DeniD-star <79475447+DeniD-star@users.noreply.github.com>
2022-07-04 12:02:09 +0200

    routing ready

--- pizza-app/src/App.js
+++ pizza-app/src/App.js
@@ -4,7 +4,6 @@ import Catalog from './pages/Catalog';
 import Contacts from './pages/Contacts'
 import HomePage from './pages/HomePage';
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -15,19 +14,16 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage/>} />
-        <Route path="/catalog" element={<Catalog />}>
-        
-        </Route>
-        <Route path="contacts" element={<Contacts />}>
-        
-        </Route>
+      <Route path="/" element={<App />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="/catalog" element={<Catalog />}>
+      </Route>
+      <Route path="contacts" element={<Contacts />}>
       </Route>
     </Routes>
-  </BrowserRouter>
+
   );
 }
 
